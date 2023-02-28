860a4c2f5a6b88fa451ed11526bf289edba2cb39
wagnerona <wagnerolarco@gmail.com>
2023-02-28 12:39:40 +0000

    Generates whole readme except license

--- index.js
+++ index.js
@@ -11,11 +11,52 @@ const questions = [
         name: 'title',
         message: 'What is the title of your project?',
     },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe your project',
+    },
+    {
+        type: 'input',
+        name: 'installs',
+        message: 'Is there anything to install for this repo to work?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Give a description on how to use this repo',
+    },
+    {
+        type: 'list',
+        name: 'usage',
+        message: 'Please select a license for this project',
+        choices: ['MIT', 'GPL', 'BSD'],
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'Any contributors for this project?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What tests did you run for this project, if any?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your GitHub username',
+    },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName,data, (err) => {
+    fs.writeFile(fileName, data, (err) => {
         err ? console.log(error) : console.log('Readme has successfully been generated!')
     })
 }
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,7 +2,46 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+  
+  ${data.description}
+  
+  ## Table of contents
+  
+  - [Installations](#installations)
+  - [Usage](#Usage)
+  - [Contributors](#Contributors)
+  - [Tests](#Tests)
+  - [Questions](#Questions)
+  - [License](#License)
+
+  ## Installations
+  
+  ${data.installs}
+  
+  ## Usage 
+
+  ${data.usage}
+
+  ## Contributors
+  
+  ${data.contributors}
+
+  ## Tests 
+
+  ${data.tests}
+  
+  ## Questions 
+
+  If you have any questions please contact me at ${data.email} or if check me out on GitHub at ${data.github}
+
+  ## License
+
+  ${data.license} 
+
 `;
 }
+
+
 
 module.exports = generateMarkdown;
